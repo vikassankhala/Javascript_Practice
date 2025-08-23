69598154730f1bca0de0d2ac8e8fb0565ee5ce68
vikassankhala <vikassankhala121@gmail.com>
2025-08-23 17:47:03 +0530

    basic

---
+++ basic/script.js
@@ -0,0 +1,51 @@
+// level 1
+console.log("hello");  //  print in console 
+
+console.warn("accha");// also print in console but give warning symbol
+alert("error h ganduu");//  it will pop up on the the screen 
+prompt("hello baby"); // it will pop up on the the screen  but also take input from user
+
+// level 3
+
+// datatype 
+// primitive dt 
+// reference dt 
+//   []  () {}   
+
+var arr=[1,2,3,4,5,6];
+var b= arr;  // this is not creating copy but it is giving refrence
+                // if one element is removed it will effect other element too
+
+ b.pop();  
+
+ // spread operator (this helps in coping a array in other array)
+var arr=[1,2,3,4,5,6];
+var b = [...arr]; //  by this you can make copy of an array
+
+// loop thst you know same as usual 
+
+// level 4 
+
+ 
+ function ab( a  ){
+    
+    return a;
+ }
+console.log(ab("vikas")) ;
+// function important hai or alag bhi h baki programming language sa
+// types 
+// es5 - function statements , function expressions, anonymous function 
+// es5 -  fat arrow function  only one function 
+// a -basic fat arrow
+// b - fat arrow with one param =>(a){}
+// c - fat  arrow with implicit return 
+            
+// by one paremeter syntax
+
+ function abc (a){  // ~ use or not paremeter 
+
+ }
+
+ // all function types by which we can create function but these all 
+   // work in same menner so only the form is different 
+             
